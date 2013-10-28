ae3fb29774c2d6381381565b1ded8a9e1a06648c
Marcus André <hello@marcusandre.de>
2013-10-28 21:44:22 +0100

    Adding and sourcing nvm. There is no easier way to manage your Node.js bins.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+# symlink all dot-files and prefix them with a dot char
 for f in `pwd`/dots/*
 do
-    ln -sf "$f" "$HOME/.${f##*/}"
+  ln -sf "$f" "$HOME/.${f##*/}"
 done
+
+# install nvm
+if [[ ! -d ~/.nvm ]]; then
+  git clone https://github.com/creationix/nvm.git ~/.nvm
+fi
+
+# reload the profile
+source ~/.bash_profile
