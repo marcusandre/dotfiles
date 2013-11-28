64a1e955baf6019f5e889877cfbe11e39455b8f3
Marcus André <hello@marcusandre.de>
2013-11-28 21:12:35 +0100

    more brew recipes.

--- brew.sh
+++ brew.sh
@@ -9,3 +9,12 @@ brew install rbenv
 brew install ruby-build
 brew install vim
 brew install wget
+brew install bash
+brew install ack
+brew install tree
+
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+# brew tap phinze/homebrew-cask
+# brew install brew-cask
