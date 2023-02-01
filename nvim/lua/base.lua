30f51264f25f992f0b1cd4d18efe0d070948863a
Marcus André <andremarcus@johndeere.com>
2023-02-01 13:29:54 +0100

    nvim: highlight yanks

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -35,3 +35,18 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.o.termguicolors = true
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
