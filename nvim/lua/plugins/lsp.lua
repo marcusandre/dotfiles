58a8f7cf703a0552e7e0f4c9ed6c52a572f4cfb0
Marcus André <hello+github@marcusandre.de>
2023-04-17 07:30:16 +0200

    nvm: lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,31 +14,46 @@ return {
       require("fidget").setup({})
 
       local servers = {
-        rust_analyzer = {},
-        tsserver = {},
+        bashls = {},
+        cssls = {},
         eslint = {},
+        html = {},
+        jsonls = {},
         lua_ls = {
           Lua = {
             workspace = { checkThirdParty = false },
             telemetry = { enable = false },
-            diagnostics = {
-              globals = {
-                "vim",
-              },
-            },
+            diagnostics = { globals = { "vim", }, },
           },
         },
+        rome = {},
+        rust_analyzer = {},
+        stylelint_lsp = {},
+        terraformls = {},
+        tsserver = {},
+        yamlls = {},
       }
 
       local mason_lspconfig = require("mason-lspconfig")
       local capabilities = vim.lsp.protocol.make_client_capabilities()
 
       local on_attach = function(client, bufnr)
+        if client.name == "tsserver" then
+          client.server_capabilities.documentFormattingProvider = false
+        end
+
         require("lsp-format").on_attach(client)
 
         vim.api.nvim_buf_create_user_command(bufnr, "Format", function()
           vim.lsp.buf.format()
         end, { desc = "Format current buffer with LSP" })
+
+        -- :EslintFixAll
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
+          command = "silent! EslintFixAll",
+          group = vim.api.nvim_create_augroup("EslintFmt", {}),
+        })
       end
 
       mason_lspconfig.setup({
@@ -61,21 +76,14 @@ return {
       keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
       keymap.set("n", "<leader>le", vim.diagnostic.open_float, { desc = "LSP: diagnostics popup" })
       keymap.set("n", "<leader>lf", "<Cmd>lua vim.lsp.buf.format({ async = true })<CR>", { desc = "LSP: format" })
-      keymap.set("n", "<space>ld", vim.lsp.buf.type_definition)
-      keymap.set("n", "<space>la", vim.lsp.buf.code_action)
-      keymap.set("n", "<space>lr", vim.lsp.buf.rename)
-      keymap.set("n", "K", vim.lsp.buf.hover)
-      keymap.set("n", "gD", vim.lsp.buf.declaration)
-      keymap.set("n", "gd", vim.lsp.buf.definition)
-      keymap.set("n", "gi", vim.lsp.buf.implementation)
-      keymap.set("n", "gr", vim.lsp.buf.references)
-
-      -- :EslintFixAll
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
-        command = "silent! EslintFixAll",
-        group = vim.api.nvim_create_augroup("EslintFmt", {}),
-      })
+      keymap.set("n", "<space>ld", vim.lsp.buf.type_definition, { desc = "LSP: type definition" })
+      keymap.set("n", "<space>la", vim.lsp.buf.code_action, { desc = "LSP: code action" })
+      keymap.set("n", "<space>lr", vim.lsp.buf.rename, { desc = "LSP: rename" })
+      keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: hover" })
+      keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: declaration" })
+      keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: definition" })
+      keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "LSP: implementation" })
+      keymap.set("n", "gr", vim.lsp.buf.references, { desc = "LSP: references" })
     end,
   },
 }
