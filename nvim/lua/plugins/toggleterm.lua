eb5d44c2558d4cb575fb940d16ef6872821668e4
Marcus André <hello+github@marcusandre.de>
2023-04-17 09:33:47 +0200

    nvim: add toggleterm

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,27 @@
+return {
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require('toggleterm').setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-\>]],
+    })
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+  end
+}
