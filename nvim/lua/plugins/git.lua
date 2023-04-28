000726e231b38129db409b0c94ce97fdbe2aa1cd
Marcus André <hello+github@marcusandre.de>
2023-04-28 07:40:16 +0200

    nvim: mappings and descriptions

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -6,6 +6,7 @@ return {
     config = function()
       require('gitsigns').setup({})
 
+      vim.keymap.set('n', '<leader>go', '<Cmd>Git<CR>', { desc = 'Git: Open (Fugitive)' })
       vim.keymap.set('n', '[h', '<Cmd>Gitsigns prev_hunk<CR>', { desc = 'Git: Previous Hunk' })
       vim.keymap.set('n', ']h', '<Cmd>Gitsigns next_hunk<CR>', { desc = 'Git: Next Hunk' })
     end,
