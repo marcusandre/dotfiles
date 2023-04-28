5c349bccc8360f12528d77273a6480e6bfa69c92
Marcus André <hello+github@marcusandre.de>
2023-04-28 07:44:50 +0200

    nvim: deactivate formatting on save
    It was such an easy and nice addition but it comes with the pain of
    a lot of unsaved buffers that I am not willing to take atm.
    
    With a little time I will tackle this again. For now I will just
    continue to use <leader>lf.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -50,11 +50,11 @@ return {
           })
         end, { desc = 'Format current buffer with LSP' })
 
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
-          command = 'silent! EslintFixAll',
-          group = vim.api.nvim_create_augroup('EslintFmt', {}),
-        })
+        -- vim.api.nvim_create_autocmd('BufWritePre', {
+        --   pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
+        --   command = 'silent! EslintFixAll',
+        --   group = vim.api.nvim_create_augroup('EslintFmt', {}),
+        -- })
       end
 
       mason_lspconfig.setup({
