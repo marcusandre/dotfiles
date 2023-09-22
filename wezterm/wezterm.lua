c1623f9784c106998b675ba2c3beb882a5e1a321
Marcus André <hello+github@marcusandre.de>
2023-09-22 07:48:54 +0200

    updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -42,6 +42,7 @@ config.enable_scroll_bar = false
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
+config.show_new_tab_button_in_tab_bar = false
 
 -- config.window_close_confirmation = "NeverPrompt"
 
