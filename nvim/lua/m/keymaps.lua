df7e07fd05e3f768ea2268d34a2de7bd75a3a777
Marcus André <hello+github@marcusandre.de>
2023-10-04 07:44:37 +0200

    nvim: add mapping for quickfix toggle

--- nvim/lua/m/keymaps.lua
+++ nvim/lua/m/keymaps.lua
@@ -6,3 +6,21 @@ vim.keymap.set("n", "Y", "y$")
 vim.keymap.set("n", "n", "nzz")
 
 vim.keymap.set("n", "<leader>z", "<Cmd>wq<cr>", { desc = "Save and Quit" })
+
+local function qf()
+  local qf_exists = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      qf_exists = true
+    end
+  end
+  if qf_exists == true then
+    vim.cmd("cclose")
+    return
+  end
+  if not vim.tbl_isempty(vim.fn.getqflist()) then
+    vim.cmd("copen")
+  end
+end
+
+vim.keymap.set("n", "<leader>c", qf, { desc = "Save and Quit" })
