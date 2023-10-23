0f0d47dde4944d90ca0abd9023b0a95760d4eea7
Marcus André <hello+github@marcusandre.de>
2023-10-23 08:21:00 +0200

    nvim: updates

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_buf_set_keymap(0, "i", "<M-i>", " = ", { noremap = true })
--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -5,10 +5,10 @@ return {
     build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
-      local ls = require("luasnip")
+      local luasnip = require("luasnip")
       local types = require("luasnip.util.types")
 
-      ls.config.set_config({
+      luasnip.config.set_config({
         history = true,
         updateevents = "TextChanged,TextChangedI",
         enable_autosnippets = true,
@@ -23,20 +23,33 @@ return {
 
       require("luasnip.loaders.from_vscode").lazy_load()
 
-      vim.keymap.set({ "i" }, "<C-K>", function()
-        ls.expand()
-      end, { silent = true })
-      vim.keymap.set({ "i", "s" }, "<C-L>", function()
-        ls.jump(1)
-      end, { silent = true })
-      vim.keymap.set({ "i", "s" }, "<C-J>", function()
-        ls.jump(-1)
-      end, { silent = true })
-      vim.keymap.set({ "i", "s" }, "<C-I>", function()
-        if ls.choice_active() then
-          ls.change_choice(1)
+      local luasnip_go_left = function()
+        if luasnip.jumpable() then
+          luasnip.jump(-1)
         end
-      end, { silent = true })
+      end
+
+      local luasnip_go_right = function()
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end
+
+      local luasnip_change_choice = function()
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end
+
+      vim.keymap.set({ "i", "s" }, "<C-H>", function()
+        luasnip_go_left()
+      end)
+      vim.keymap.set({ "i", "s" }, "<C-L>", function()
+        luasnip_go_right()
+      end)
+      vim.keymap.set({ "i", "s" }, "<C-I>", function()
+        luasnip_change_choice()
+      end)
     end,
   },
 }
