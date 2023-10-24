2cedec5a2ab692e5dc973a0aed5c723369074476
Marcus André <hello+github@marcusandre.de>
2023-10-24 13:56:47 +0200

    nvim: update keymaps

--- nvim/lua/m/keymaps.lua
+++ nvim/lua/m/keymaps.lua
@@ -1,9 +1,11 @@
 local fns = require("m.functions")
 
 vim.keymap.set("n", "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+vim.keymap.set("n", "<leader>bs", fns.make_scratch_buffer, { desc = "Scratch" })
+vim.keymap.set("n", "<leader>oc", fns.toggle_quickfix, { desc = "Quickfix: Toggle" })
+vim.keymap.set("n", "<leader>oo", "<Cmd>only<CR>", { desc = "Window: Only" })
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "n", "nzz")
+
 vim.keymap.set("i", "kj", "<esc>")
-vim.keymap.set("n", "<leader>oc", fns.toggle_quickfix, { desc = "Quickfix: Toggle" })
-vim.keymap.set("n", "<leader>bs", fns.make_scratch_buffer, { desc = "Scratch" })
