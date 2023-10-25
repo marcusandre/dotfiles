54c86237c5eb0524a8ebcbbc991e158f1eeaf336
Marcus André <hello+github@marcusandre.de>
2023-10-25 23:35:11 +0200

    nvim: update lua ftplugin

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -2,16 +2,19 @@
 vim.api.nvim_buf_set_keymap(0, "i", "<M-i>", " = ", { noremap = true })
 
 -- Handle trainling comma when splitting inside curly brackets
-local MiniSplitjoin = require("mini.splitjoin")
-local gen_hook = MiniSplitjoin.gen_hook
-local curly = { brackets = { "%b{}" } }
+local status, MiniSplitjoin = pcall(require, "mini.splitjoin")
 
-local add_comma_curly = gen_hook.add_trailing_separator(curly)
-local del_comma_curly = gen_hook.del_trailing_separator(curly)
-local pad_curly = gen_hook.pad_brackets(curly)
+if status then
+  local gen_hook = MiniSplitjoin.gen_hook
+  local curly = { brackets = { "%b{}" } }
 
----@diagnostic disable-next-line: inject-field
-vim.b.minisplitjoin_config = {
-  split = { hooks_post = { add_comma_curly } },
-  join = { hooks_post = { del_comma_curly, pad_curly } },
-}
+  local add_comma_curly = gen_hook.add_trailing_separator(curly)
+  local del_comma_curly = gen_hook.del_trailing_separator(curly)
+  local pad_curly = gen_hook.pad_brackets(curly)
+
+  ---@diagnostic disable-next-line: inject-field
+  vim.b.minisplitjoin_config = {
+    split = { hooks_post = { add_comma_curly } },
+    join = { hooks_post = { del_comma_curly, pad_curly } },
+  }
+end
