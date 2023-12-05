d056277942d51fc7fce93443f43cfa23584d510e
Marcus André <hello+github@marcusandre.de>
2023-12-05 11:43:48 +0100

    fish: add b command to boot neovim beta branch

--- fish/config.fish
+++ fish/config.fish
@@ -82,6 +82,7 @@ alias .. 'cd ..'
 alias cat bat
 alias cloc 'tokei'
 alias e nvim
+alias b 'NVIM_APPNAME=beta nvim'
 alias flushdns 'sudo killall -HUP mDNSResponder'
 alias gap 'git add . -p'
 alias gb 'git branch -a'
