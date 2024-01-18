faeb437272c0ef5d52d983a28e08c35bba745556
Marcus André <hello+github@marcusandre.de>
2024-01-18 23:23:32 +0100

    fish: vivid theme

--- fish/config.fish
+++ fish/config.fish
@@ -8,13 +8,13 @@ set fish_greeting
 fish_add_path "$HOME/bin"
 
 if status --is-interactive
-  if test -x /opt/homebrew/bin/brew
-    eval (/opt/homebrew/bin/brew shellenv)
-  end
+    if test -x /opt/homebrew/bin/brew
+        eval (/opt/homebrew/bin/brew shellenv)
+    end
 
-  if test -x /usr/local/bin/brew
-    eval (/usr/local/bin/brew shellenv)
-  end
+    if test -x /usr/local/bin/brew
+        eval (/usr/local/bin/brew shellenv)
+    end
 end
 
 # Env
@@ -23,40 +23,38 @@ set -gx BROWSER chromium
 set -gx VISUAL nvim
 set -gx PAGER "less -R --use-color -Dd+r -Du+b"
 set -gx MANPAGER 'nvim +Man!'
-set -gx NVIM_LISTEN_ADDRESS "/tmp/nvimsocket"
+set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
 set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/.ripgreprc"
-# set -gx LS_COLORS "vivid generate catppuccin-macchiato"
+set -gx LS_COLORS "vivid generate snazzy"
 
 # Go
-set -gx GOPATH $HOME/golang
-
-if test -d $HOME/golang/bin
-    fish_add_path "$HOME/golang/bin"
+if test -d $HOME/go/bin
+    fish_add_path "$HOME/go/bin"
 end
 
 # OCaml
 if test -e $HOME/.opam/opam-init/init.fish
-  source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+    source $HOME/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
 end
 
 # cargo
 if test -d $HOME/.cargo/bin
-  fish_add_path "$HOME/.cargo/bin"
+    fish_add_path "$HOME/.cargo/bin"
 end
 
 # zoxide
 if type -q zoxide
-  zoxide init fish | source
+    zoxide init fish | source
 end
 
 # mise
 if type -q mise
-  mise activate fish | source
+    mise activate fish | source
 end
 
 # prompt
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
 
 # Create a directory and set CWD
@@ -76,7 +74,7 @@ end
 # Aliases
 alias .. 'cd ..'
 alias cat bat
-alias cloc 'tokei'
+alias cloc tokei
 alias e nvim
 alias flushdns 'sudo killall -HUP mDNSResponder'
 alias gap 'git add . -p'
@@ -86,19 +84,19 @@ alias gl 'git ll'
 alias gp "git symbolic-ref --short HEAD 2> /dev/null | xargs -L1 git push origin"
 alias gr 'cd $(git root)'
 alias gu 'git upgrade'
-alias lg 'lazygit'
+alias lg lazygit
 alias ll 'ls -la'
 alias ls eza
 alias rf 'rm -fr'
 alias s 'git s'
-alias sloc 'tokei'
+alias sloc tokei
 alias ss 'git status'
 alias tree 'eza --tree'
 alias vimdiff 'nvim -d'
 
 # walk
 function lk
-  set loc (walk $argv); and cd $loc;
+    set loc (walk $argv); and cd $loc
 end
 
-fish_config theme choose "tokyonight_moon"
+fish_config theme choose tokyonight_moon
