98089cb60ccacbba4ba136878eee240fcf0b4b24
Marcus André <hello+github@marcusandre.de>
2024-02-02 12:59:44 +0100

    nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    keys = {
+      { '-', '<Cmd>Oil<CR>', desc = 'Open parent directory' },
+    },
+    config = function()
+      local oil = require('oil')
+
+      oil.setup({
+        default_file_explorer = true,
+        delete_to_trash = false,
+        lsp_rename_autosave = true,
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
+  },
+}
