761011c034c76f9aaa55ae54402c691ceef17bf3
Marcus André <hello+github@marcusandre.de>
2024-02-22 23:06:21 +0100

    .

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   {
     'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
       { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
       'saadparwaiz1/cmp_luasnip',
@@ -17,6 +18,7 @@ return {
     },
     config = function()
       local cmp = require('cmp')
+      local compare = require('cmp.config.compare')
       local luasnip = require('luasnip')
       local lspkind = require('lspkind')
 
@@ -29,14 +31,12 @@ return {
           expand = function(args) luasnip.lsp_expand(args.body) end,
         },
         completion = {
-          completeopt = 'menu,menuone,noinsert',
+          completeopt = 'menu,menuone,noinsert,noselect',
         },
         formatting = {
           format = lspkind.cmp_format({}),
         },
         mapping = cmp.mapping.preset.insert({
-          -- ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete({}),
@@ -63,10 +63,22 @@ return {
             end
           end, { 'i', 's' }),
         }),
-        sources = {
+        sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+        }, {
+          { name = 'buffer', keyword_length = 4 },
+        }),
+        sorting = {
+          comparators = {
+            compare.offset,
+            compare.exact,
+            compare.score,
+            compare.recently_used,
+            compare.sort_text,
+            compare.length,
+          },
         },
       })
     end,
