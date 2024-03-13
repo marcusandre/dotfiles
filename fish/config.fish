000737000ed69b04fa577c45ca609556ba23449f
Marcus André <hello+github@marcusandre.de>
2024-03-13 15:27:42 +0100

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,10 @@ set -U fish_greeting
 
 set -x LC_ALL en_US.UTF-8
 
-set -x GIT_EDITOR nvim
-set -x EDITOR nvim
-set -x VISUAL nvim
+set -x GIT_EDITOR "nvim"
+set -x EDITOR "nvim"
+set -x VISUAL "nvim"
+set -x MANPAGER "nvim -c 'set ft=man' -"
 
 set -x GOPATH $HOME
 
@@ -45,7 +46,6 @@ end
 alias l="ls -a"
 alias ll="ls -la"
 alias cat='bat --paging=never'
-
 alias rf="rm -fr"
 alias e="nvim"
 
@@ -57,3 +57,11 @@ alias gp="git push"
 alias gu="git upgrade"
 alias gr="cd (git rev-parse --show-toplevel)"
 alias gap="git add . -p"
+
+function ef --description "Skim through files and open them in nvim"
+    rg --files | sk --preview="bat {} --color=always" | xargs -r nvim
+end
+
+function eg --description "Skim through lines and open them in nvim"
+    sk -m --ansi -i -c 'rg --line-number  "{}"'| awk -F':' '{print "+"$2, $1}' | xargs -r nvim
+end
