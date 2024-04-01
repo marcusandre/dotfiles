a7f736621aead8054158a2a80eff9f890419e6d5
Marcus André <hello+github@marcusandre.de>
2024-04-01 23:34:15 +0200

    molokai statusline

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -21,7 +21,15 @@ return {
     'tomasr/molokai',
     lazy = false,
     priority = 1000,
-    config = function() vim.cmd([[colorscheme molokai]]) end,
+    config = function()
+      vim.cmd([[colorscheme molokai]])
+
+      -- mini.statusline
+      vim.api.nvim_set_hl(0, 'MiniStatuslineModeNormal', { bg = '#4C4745', fg = '#FFFFFF' })
+      vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert', { bg = '#AE81FF', fg = '#FFFFFF' })
+      vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { bg = '#4C4745', fg = '#89807D' })
+      vim.api.nvim_set_hl(0, 'MiniStatuslineFileinfo', { bg = '#4C4745', fg = '#89807D' })
+    end,
   },
   {
     'ellisonleao/gruvbox.nvim',
