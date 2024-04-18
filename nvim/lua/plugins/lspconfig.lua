15c52902c51cedce5407d8516a89432c03e8a3a6
Marcus André <hello+github@marcusandre.de>
2024-04-18 13:29:45 +0200

    inlay hints

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -219,28 +219,28 @@ return { -- LSP Configuration & Plugins
       --   end
       -- end
 
-      -- if client.supports_method('textDocument/inlayHint') then
-      --   local inlay_hints_group = vim.api.nvim_create_augroup('man-toggle_inlay_hints', { clear = false })
-      --
-      --   vim.defer_fn(function()
-      --     local mode = vim.api.nvim_get_mode().mode
-      --     vim.lsp.inlay_hint.enable(bufnr, mode == 'n' or mode == 'v')
-      --   end, 500)
-      --
-      --   vim.api.nvim_create_autocmd('InsertEnter', {
-      --     group = inlay_hints_group,
-      --     desc = 'Enable inlay hints',
-      --     buffer = bufnr,
-      --     callback = function() vim.lsp.inlay_hint.enable(bufnr, false) end,
-      --   })
-      --
-      --   vim.api.nvim_create_autocmd('InsertLeave', {
-      --     group = inlay_hints_group,
-      --     desc = 'Disable inlay hints',
-      --     buffer = bufnr,
-      --     callback = function() vim.lsp.inlay_hint.enable(bufnr, true) end,
-      --   })
-      -- end
+      if client.supports_method('textDocument/inlayHint') then
+        local inlay_hints_group = vim.api.nvim_create_augroup('man-toggle_inlay_hints', { clear = false })
+
+        vim.defer_fn(function()
+          local mode = vim.api.nvim_get_mode().mode
+          vim.lsp.inlay_hint.enable(bufnr, mode == 'n' or mode == 'v')
+        end, 500)
+
+        vim.api.nvim_create_autocmd('InsertEnter', {
+          group = inlay_hints_group,
+          desc = 'Enable inlay hints',
+          buffer = bufnr,
+          callback = function() vim.lsp.inlay_hint.enable(bufnr, false) end,
+        })
+
+        vim.api.nvim_create_autocmd('InsertLeave', {
+          group = inlay_hints_group,
+          desc = 'Disable inlay hints',
+          buffer = bufnr,
+          callback = function() vim.lsp.inlay_hint.enable(bufnr, true) end,
+        })
+      end
     end
 
     require('mason').setup()
