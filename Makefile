a7e34dc3e1d5a4b564e86a998aa17ea618729a86
Marcus André <hello+github@marcusandre.de>
2024-05-22 16:11:25 +0200

    chore: cleanup

--- Makefile
+++ Makefile
@@ -1,19 +1,10 @@
 CONFIG_PATH=${HOME}/.config
 
-lint:
-	stylua .
-
-all:
-	@stow -v etc vim
+link:
+	@stow -v etc
 
 	@mkdir -p $(HOME)/bin
 	@stow -v --target=$(HOME)/bin bin
-
-	@mkdir -p $(CONFIG_PATH)/atuin
-	@stow -v --adopt --target=$(CONFIG_PATH)/atuin atuin
-
-	@mkdir -p $(CONFIG_PATH)/bat
-	@stow -v --target=$(CONFIG_PATH)/bat bat
 
 	@mkdir -p $(CONFIG_PATH)/fish
 	@stow -v --target=$(CONFIG_PATH)/fish fish
@@ -21,24 +12,13 @@ all:
 	@mkdir -p $(CONFIG_PATH)/git
 	@stow -v --target=$(CONFIG_PATH)/git git
 
-	@mkdir -p $(CONFIG_PATH)/mise
-	@stow -v --target=$(CONFIG_PATH)/mise mise
-
 	@mkdir -p $(CONFIG_PATH)/jj
 	@stow -v --target=$(CONFIG_PATH)/jj jj
 
 	@mkdir -p $(CONFIG_PATH)/helix
 	@stow -v --target=$(CONFIG_PATH)/helix helix
 
-	@mkdir -p $(CONFIG_PATH)/ripgrep
-	@stow -v --target=$(CONFIG_PATH)/ripgrep ripgrep
-
-	@mkdir -p $(HOME)/Library/Application\ Support/nushell
-	@stow -v --target=$(HOME)/Library/Application\ Support/nushell nushell
-
 	@mkdir -p $(CONFIG_PATH)/wezterm
 	@stow -v --target=$(CONFIG_PATH)/wezterm wezterm
 
-	# @mkdir -p $(CONFIG_PATH)/zed
-	# @stow -v --target=$(CONFIG_PATH)/zed zed
-.PHONY: all lint
+.PHONY: link
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- General
-config.term = 'wezterm'
+config.term = "wezterm"
 
 -- Text
 -- config.font = wezterm.font('JetBrains Mono')
-config.font = wezterm.font('Berkeley Mono')
+config.font = wezterm.font("Berkeley Mono")
 -- config.font = wezterm.font('Fira Code')
 -- config.font = wezterm.font('Input Mono Narrow')
 config.font_size = 14
@@ -16,13 +16,13 @@ config.font_size = 14
 config.initial_rows = 54
 config.initial_cols = 180
 
-config.term = 'wezterm'
+config.term = "wezterm"
 config.set_environment_variables = {
-  TERMINFO_DIRS = wezterm.home_dir .. '/.terminfo',
+  TERMINFO_DIRS = wezterm.home_dir .. "/.terminfo",
 }
 
 -- Colors
-config.color_scheme = 'nightfox'
+config.color_scheme = "nightfox"
 -- config.color_scheme = 'tokyonight_storm'
 -- config.color_scheme = 'Poimandres'
 -- config.color_scheme = 'Molokai (Gogh)'
@@ -44,22 +44,22 @@ config.window_padding = {
 local action = wezterm.action
 
 config.keys = {
-  { mods = 'ALT', key = 'Enter', action = 'DisableDefaultAssignment' },
-  { mods = 'ALT', key = '\\', action = action.ShowTabNavigator },
-  { mods = 'CMD', key = ',', action = action.SplitHorizontal({ domain = 'CurrentPaneDomain' }) },
-  { mods = 'CMD', key = '.', action = action.SplitVertical({ domain = 'CurrentPaneDomain' }) },
-  { mods = 'CMD', key = 'DownArrow', action = action.AdjustPaneSize({ 'Down', 5 }) },
-  { mods = 'CMD', key = 'LeftArrow', action = action.AdjustPaneSize({ 'Left', 5 }) },
-  { mods = 'CMD', key = 'RightArrow', action = action.AdjustPaneSize({ 'Right', 5 }) },
-  { mods = 'CMD', key = 'UpArrow', action = action.AdjustPaneSize({ 'Up', 5 }) },
-  { mods = 'CMD', key = '\\', action = wezterm.action.ToggleFullScreen },
-  { mods = 'CMD', key = 'e', action = 'ActivateCopyMode' },
-  { mods = 'CMD', key = 'h', action = action.ActivatePaneDirection('Left') },
-  { mods = 'CMD', key = 'j', action = action.ActivatePaneDirection('Down') },
-  { mods = 'CMD', key = 'k', action = action.ActivatePaneDirection('Up') },
-  { mods = 'CMD', key = 'l', action = action.ActivatePaneDirection('Right') },
-  { mods = 'CMD', key = 'm', action = 'DisableDefaultAssignment' },
-  { mods = 'CMD', key = 'w', action = action.CloseCurrentPane({ confirm = true }) },
+  { mods = "ALT", key = "Enter", action = "DisableDefaultAssignment" },
+  { mods = "ALT", key = "\\", action = action.ShowTabNavigator },
+  { mods = "CMD", key = ",", action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { mods = "CMD", key = ".", action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { mods = "CMD", key = "DownArrow", action = action.AdjustPaneSize({ "Down", 5 }) },
+  { mods = "CMD", key = "LeftArrow", action = action.AdjustPaneSize({ "Left", 5 }) },
+  { mods = "CMD", key = "RightArrow", action = action.AdjustPaneSize({ "Right", 5 }) },
+  { mods = "CMD", key = "UpArrow", action = action.AdjustPaneSize({ "Up", 5 }) },
+  { mods = "CMD", key = "\\", action = wezterm.action.ToggleFullScreen },
+  { mods = "CMD", key = "e", action = "ActivateCopyMode" },
+  { mods = "CMD", key = "h", action = action.ActivatePaneDirection("Left") },
+  { mods = "CMD", key = "j", action = action.ActivatePaneDirection("Down") },
+  { mods = "CMD", key = "k", action = action.ActivatePaneDirection("Up") },
+  { mods = "CMD", key = "l", action = action.ActivatePaneDirection("Right") },
+  { mods = "CMD", key = "m", action = "DisableDefaultAssignment" },
+  { mods = "CMD", key = "w", action = action.CloseCurrentPane({ confirm = true }) },
 }
 
 return config
