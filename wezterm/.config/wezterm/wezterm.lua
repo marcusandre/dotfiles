397dd4737880299a50a84c65ceb4bcd2c14513af
Marcus André <hello+github@marcusandre.de>
2024-07-11 22:49:11 +0200

    wezterm: update color settings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -30,9 +30,9 @@ config.set_environment_variables = {
 -- Colors
 local scheme_for_appearance = function(appearance)
   if appearance:find("Dark") then
-    return "nightfox"
+    return "tokyonight_night"
   else
-    return "nightfox"
+    return "tokyonight_night"
   end
 end
 
