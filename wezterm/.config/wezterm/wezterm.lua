b700f3011cfe474fba99e47f10964cf3ff784323
Marcus André <hello+github@marcusandre.de>
2024-07-15 15:42:41 +0200

    wezterm: update color settings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -30,9 +30,9 @@ config.set_environment_variables = {
 -- Colors
 local scheme_for_appearance = function(appearance)
   if appearance:find("Dark") then
-    return "tokyonight_storm"
+    return "Gruvbox dark, medium (base16)"
   else
-    return "tokyonight_storm"
+    return "Gruvbox dark, medium (base16)"
   end
 end
 
