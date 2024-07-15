c4af4d514cbcdd508ca4f75e2959a1df46b1eedf
Marcus André <hello+github@marcusandre.de>
2024-07-15 15:51:58 +0200

    misc: update color settings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -54,7 +54,7 @@ if type -q fzf
     fzf --fish | source
 
     if command -sq bat
-        set -gx FZF_CTRL_T_OPTS "--ansi --preview 'bat -f -p --theme ansi --line-range :300 {}'"
+        set -gx FZF_CTRL_T_OPTS "--ansi --preview 'bat -f -p --line-range :300 {}'"
     else
         set -gx FZF_CTRL_T_OPTS "--preview 'cat {}'"
     end
