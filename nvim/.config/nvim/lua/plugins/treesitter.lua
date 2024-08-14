a44332dc4d70fec5657d8c57ca874a2e08a1d4f1
Marcus André <hello+github@marcusandre.de>
2024-08-14 14:23:52 +0200

    nvim: fmt

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -43,9 +43,7 @@ return {
       local max_filesize = 100 * 1024 -- 100 KB
       ---@diagnostic disable-next-line: undefined-field
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
+      if ok and stats and stats.size > max_filesize then return true end
     end
 
     ---@diagnostic disable-next-line: missing-fields
