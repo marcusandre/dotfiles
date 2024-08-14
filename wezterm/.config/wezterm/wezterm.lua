ef59186855fa1f89081cca8e5a77b9ba2b99a45a
Marcus André <hello+github@marcusandre.de>
2024-08-14 17:23:03 +0200

    Revert 1 commits
    2ef042f 'nvim,wezterm,zed: update colors'

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -30,9 +30,9 @@ config.set_environment_variables = {
 -- Colors
 local scheme_for_appearance = function(appearance)
   if appearance:find("Dark") then
-    return "carbonfox"
+    return "duskfox"
   else
-    return "carbonfox"
+    return "duskfox"
   end
 end
 
