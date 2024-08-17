75cd1f4d8dedc1658daabd75b8104c4f47e4b36a
Marcus André <hello+github@marcusandre.de>
2024-08-17 08:26:44 +0200

    nvim: update fzf keybindings

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -2,6 +2,7 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons", opts = {} },
   keys = {
+    { "<leader>fa", "<Cmd>FzfLua git_status<CR>", desc = "Git status" },
     { "<leader>fb", "<Cmd>FzfLua buffers<CR>", desc = "Buffers" },
     { "<leader>fc", "<Cmd>FzfLua quickfix<CR>", desc = "Quickfix" },
     { "<leader>ff", "<Cmd>FzfLua files<CR>", desc = "Files" },
@@ -11,7 +12,6 @@ return {
     { "<leader>fL", "<Cmd>FzfLua lines<CR>", desc = "Lines" },
     { "<leader>fo", "<Cmd>FzfLua oldfiles<CR>", desc = "Old files" },
     { "<leader>fR", "<Cmd>FzfLua resume<CR>", desc = "Resume" },
-    { "<leader>fs", "<Cmd>FzfLua git_status<CR>", desc = "Git status" },
     { "<leader>fw", "<Cmd>FzfLua grep_cword<CR>", desc = "Grep word" },
     { "<leader>fx", "<Cmd>FzfLua builtin<CR>", desc = "Builtin" },
     { "<leader>fz", "<Cmd>FzfLua spell_suggest<CR>", desc = "Spelling" },
