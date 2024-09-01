0e17b71c691fbd40e6a70f9f7ac5ee5a79059670
Marcus André <hello+github@marcusandre.de>
2024-09-01 19:47:44 +0200

    nvim: add treesj

---
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,6 @@
+return {
+  "Wansmer/treesj",
+  keys = { "<space>m", "<space>j", "<space>s" },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function() require("treesj").setup({}) end,
+}
