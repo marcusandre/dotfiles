0875f22028b8c0c8b074b440a076e9f087a40983
Marcus André <hello+github@marcusandre.de>
2024-09-01 23:16:28 +0200

    nvim: keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -34,19 +34,19 @@ map("n", "<leader>ft", "<Cmd>TodoTelescope<CR>", { desc = "Todo" })
 map("n", "<leader>gg", "<Cmd>Neogit<CR>", { desc = "Neogit" })
 
 -- "l" stands for LSP
-map("n", "<leader>ld", "<Cmd>Telescope diagnostics<cr>", { desc = "diagnostics", buffer = 0 })
-map("n", "<leader>le", vim.diagnostic.open_float, { desc = "Diagnostics", buffer = 0 })
-map("n", "<leader>li", "<Cmd>Telescope lsp_implementations<CR>", { desc = "Implementations", buffer = 0 })
-map("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename", buffer = 0 })
-map("n", "<leader>ls", "<Cmd>Telescope lsp_document_symbols<CR>", { desc = "Symbols (buffer)", buffer = 0 })
-map("n", "<leader>lt", "<Cmd>FzfLua lsp_typedefs<CR>", { desc = "Type Definitions", buffer = 0 })
-map("n", "<leader>lw", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", { desc = "Symbols (workspace)", buffer = 0 })
-map("n", "<leader>lx", "<Cmd>lua vim.diagnostic.setloclist()<CR>", { desc = "Diagnostics (LocList)", buffer = 0 })
+map("n", "<leader>ld", "<Cmd>Telescope diagnostics<cr>", { desc = "diagnostics" })
+map("n", "<leader>le", vim.diagnostic.open_float, { desc = "Diagnostics" })
+map("n", "<leader>li", "<Cmd>Telescope lsp_implementations<CR>", { desc = "Implementations" })
+map("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
+map("n", "<leader>ls", "<Cmd>Telescope lsp_document_symbols<CR>", { desc = "Symbols (buffer)" })
+map("n", "<leader>lt", "<Cmd>FzfLua lsp_typedefs<CR>", { desc = "Type Definitions" })
+map("n", "<leader>lw", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", { desc = "Symbols (workspace)" })
+map("n", "<leader>lx", "<Cmd>lua vim.diagnostic.setloclist()<CR>", { desc = "Diagnostics (LocList)" })
 
 -- Related LSP mappings
-map("n", "gd", "<Cmd>Telescope lsp_definitions<CR>", { buffer = 0, desc = "Definitions" })
-map("n", "gr", "<Cmd>Telescope lsp_references<CR>", { buffer = 0, desc = "References" })
-map({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { buffer = 0, desc = "Code action" })
+map("n", "gd", "<Cmd>Telescope lsp_definitions<CR>", { desc = "Definitions" })
+map("n", "gr", "<Cmd>Telescope lsp_references<CR>", { desc = "References" })
+map({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code action" })
 
 -- "t" stands for testing
 map("n", "tf", "<Cmd>TestFile<CR>", { desc = "File" })
