15ad51184afc710df7c284b131a8ff3e4a80f984
Marcus André <hello+github@marcusandre.de>
2024-09-01 22:49:07 +0200

    nvim: update indentmini highlights

--- nvim/.config/nvim/lua/plugins/indentmini.lua
+++ nvim/.config/nvim/lua/plugins/indentmini.lua
@@ -1,4 +1,10 @@
 return {
   "nvimdev/indentmini.nvim",
-  config = function() require("indentmini").setup() end,
+  config = function()
+    require("indentmini").setup()
+
+    -- Kanagawa highlights
+    vim.cmd.highlight("IndentLine guifg=#54546d")
+    vim.cmd.highlight("IndentLineCurrent guifg=#938aa9")
+  end,
 }
