c2612e026aa21823db7f2cf5d6502c037a2302f5
Marcus André <hello+github@marcusandre.de>
2024-09-03 09:47:11 +0200

    nvim: add snippet support

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -6,37 +6,43 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    "garymjr/nvim-snippets",
   },
   opts = function()
     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
     local cmp = require("cmp")
     local defaults = require("cmp.config.default")()
-    local auto_select = true
 
     return {
       auto_brackets = {},
-      completion = {
-        completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
-      },
-      preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+      completion = { completeopt = "menu,menuone,noinsert" },
       mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        -- Select items
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+
+        -- Trigger completion manually
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = auto_select }),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
+
+        -- Accept completion
+        ["<CR>"] = cmp.mapping.confirm(),
+
+        -- Abort completion
         ["<C-CR>"] = function(fallback)
           cmp.abort()
           fallback()
         end,
+
+        -- Scroll docs
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
       }),
       sources = cmp.config.sources({
         { name = "lazydev", group_index = 0 },
         { name = "nvim_lsp" },
         { name = "path" },
-      }, {
         { name = "buffer" },
+        { name = "snippets" },
       }),
       experimental = {
         ghost_text = {
