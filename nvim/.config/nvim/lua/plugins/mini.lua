430d54ae460bce256b2ac2fc62aaffcd21972db4
Marcus André <hello+github@marcusandre.de>
2024-09-13 13:14:06 +0200

    nvim: add mini.icons if nerd font available

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,13 +1,19 @@
 return {
-  "echasnovski/mini.nvim",
-  version = false,
-  config = function()
-    require("mini.bufremove").setup()
-    require("mini.diff").setup()
-    require("mini.extra").setup()
-    require("mini.tabline").setup()
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require("mini.bufremove").setup()
+      require("mini.diff").setup()
+      require("mini.extra").setup()
+      require("mini.tabline").setup()
 
-    require("mini.pick").setup()
-    vim.ui.select = MiniPick.ui_select
-  end,
+      require("mini.pick").setup()
+      vim.ui.select = MiniPick.ui_select
+    end,
+  },
+  {
+    "echasnovski/mini.icons",
+    enabled = vim.g.have_nerd_font,
+  },
 }
