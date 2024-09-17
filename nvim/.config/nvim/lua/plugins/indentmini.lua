df94ea45683aecec045694d8574ae2e4f8169bdf
Marcus André <hello+github@marcusandre.de>
2024-09-17 17:05:31 +0200

    WIP

--- nvim/.config/nvim/lua/plugins/indentmini.lua
+++ nvim/.config/nvim/lua/plugins/indentmini.lua
@@ -4,6 +4,10 @@ return {
   config = function()
     require("indentmini").setup()
 
+    -- Duskfox highlights
+    -- vim.cmd.highlight("IndentLine guifg=#4b4673")
+    -- vim.cmd.highlight("IndentLineCurrent guifg=#6e6a86")
+
     -- Kanagawa highlights
     -- vim.cmd.highlight("IndentLine guifg=#54546d")
     -- vim.cmd.highlight("IndentLineCurrent guifg=#938aa9")
