741c5934aa1323fee915e4ece22a5b11ca79ed6a
Marcus André <hello+github@marcusandre.de>
2024-09-17 16:37:50 +0200

    nvim: use conform for `gq`

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,30 +1,28 @@
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
-  opts = {},
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        fish = { "fish_indent" },
-        gitconfig = { "squeeze_blanks", "indent_expr" },
-        gleam = { "gleam" },
-        go = { "gofumpt", "goimports", "golines" },
-        hcl = { "hcl" },
-        just = { "just", "squeeze_blanks", "indent_expr" },
-        lua = { "stylua" },
-        markdown = { "markdown-toc", "markdownlint" },
-        nix = { "nixfmt" },
-        query = { "format-queries" },
-        roc = { "roc" },
-        rust = { "rustfmt" },
-        terraform = { "terraform_fmt" },
-        toml = { "taplo" },
-        zig = { "zigfmt" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    })
-  end,
+  opts = {
+    formatters_by_ft = {
+      fish = { "fish_indent" },
+      gitconfig = { "squeeze_blanks", "indent_expr" },
+      gleam = { "gleam" },
+      go = { "gofumpt", "goimports", "golines" },
+      hcl = { "hcl" },
+      just = { "just", "squeeze_blanks", "indent_expr" },
+      lua = { "stylua" },
+      markdown = { "markdown-toc", "markdownlint" },
+      nix = { "nixfmt" },
+      query = { "format-queries" },
+      roc = { "roc" },
+      rust = { "rustfmt" },
+      terraform = { "terraform_fmt" },
+      toml = { "taplo" },
+      zig = { "zigfmt" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+  init = function() vim.o.formatexpr = "v:lua.require'conform'.formatexpr()" end,
 }
