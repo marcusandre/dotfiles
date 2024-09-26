b95008fcdf02940329ba65d88037d0e6809e8952
Marcus André <hello+github@marcusandre.de>
2024-09-26 18:21:59 +0200

    nvim: add CopilotChat

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,24 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function() require("copilot").setup() end,
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function() require("copilot").setup() end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    build = "make tiktoken",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    keys = {
+      { "<leader>cc", "<Cmd>CopilotChatToggle<CR>", desc = "Open copilot chat", mode = { "n", "v" } },
+      { "<leader>ce", "<Cmd>CopilotChatExplain<CR>", desc = "Open copilot explain panel", mode = { "n", "v" } },
+      { "<leader>cf", "<Cmd>CopilotChatFix<CR>", desc = "Open copilot fix panel", mode = { "n", "v" } },
+      { "<leader>cg", "<Cmd>CopilotChatCommit<CR>", desc = "Open copilot commit panel", mode = { "n", "v" } },
+    },
+    opts = {},
+  },
 }
