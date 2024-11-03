3c3059e44475057423454612a4981914e224ea7d
Marcus André <hello+github@marcusandre.de>
2024-11-03 14:03:35 +0100

    nvim: add lsp_lines

---
+++ nvim/.config/nvim/lua/plugins/lsp_lines.lua
@@ -0,0 +1,18 @@
+return {
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  lazy = false,
+  keys = {
+    {
+      "<leader>ol",
+      function() require("lsp_lines").toggle() end,
+      desc = "Toggle lsp_lines",
+    },
+  },
+  config = function()
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
+
+    require("lsp_lines").setup()
+  end,
+}
