0ebd52ac3a1f87a6ee9a0de1a599c19f75d826c7
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-08 21:40:00 +0100

    nvim: update snacks config

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,10 +3,15 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  opts = {},
+  opts = {
+    gitbrowse = { what = "file" },
+  },
   keys = {
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
     { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+    { "<leader>X", function() Snacks.bufdelete.other() end, desc = "Close other buffers" },
+    { "<leader>t", function() Snacks.terminal.toggle() end, desc = "Toggle terminal" },
     { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference" },
     { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference" },
   },
