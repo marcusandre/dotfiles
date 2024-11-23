2fc26ce91f501e7573dd4b3ddef7ca02749bac69
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-23 16:10:46 +0100

    git: add abbreviations

---
+++ fish/.config/fish/conf.d/30-git.fish
@@ -0,0 +1,8 @@
+if status --is-interactive
+    if type -q git
+        abbr -a gd "git diff"
+        abbr -a gp "git push"
+        abbr -a gs "git status --short"
+        abbr -a gl "git log"
+    end
+end
