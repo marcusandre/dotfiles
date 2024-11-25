8e775aea9d540ff33f7ae81f56a76b0ae0b109e2
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-25 16:58:55 +0100

    git: add aliases

--- fish/.config/fish/conf.d/30-git.fish
+++ fish/.config/fish/conf.d/30-git.fish
@@ -1,8 +1,9 @@
 if status --is-interactive
     if type -q git
         abbr -a gd "git diff"
-        abbr -a gp "git push"
-        abbr -a gs "git status --short"
         abbr -a gl "git log --oneline --graph --decorate --branches"
+        abbr -a gp "git push"
+        abbr -a gr "cd (git root)"
+        abbr -a gs "git status --short"
     end
 end
