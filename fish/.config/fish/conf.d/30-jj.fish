9f747da23691fcd75a75536d23d2edfb3ab50474
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-25 10:07:06 +0100

    jj: add another abbr

--- fish/.config/fish/conf.d/30-jj.fish
+++ fish/.config/fish/conf.d/30-jj.fish
@@ -1,7 +1,8 @@
 if status --is-interactive
     if type -q jj
         abbr -a jd "jj diff"
-        
+        abbr -a js "jj status"
+
         set -gx JJ_CONFIG "$HOME/.config/jj/config.toml"
         jj util completion fish | source
     end
