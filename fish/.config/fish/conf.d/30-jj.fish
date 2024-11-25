c243db53f85fe4d8e05b97367f2627bb1b050ee1
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-25 09:12:47 +0100

    jj: add `js` abbreviation

--- fish/.config/fish/conf.d/30-jj.fish
+++ fish/.config/fish/conf.d/30-jj.fish
@@ -1,5 +1,7 @@
 if status --is-interactive
     if type -q jj
+        abbr -a js "jj status"
+
         set -gx JJ_CONFIG "$HOME/.config/jj/config.toml"
         jj util completion fish | source
     end
