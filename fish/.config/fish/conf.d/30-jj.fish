411ce25b82434909a1d9a6dce6cae9ee3912ad46
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-27 23:12:27 +0100

    fish: remove jj keybinding because of console errors

--- fish/.config/fish/conf.d/30-jj.fish
+++ fish/.config/fish/conf.d/30-jj.fish
@@ -6,16 +6,16 @@ if status --is-interactive
         set -gx JJ_CONFIG "$HOME/.config/jj/config.toml"
         jj util completion fish | source
 
-        if type -q fzf
-            function fzf_jj_edit
-                if test -d .jj
-                    set -l name (jj log --no-graph -T 'change_id.shortest() ++ "\t" ++ description.first_line() ++ " "  ++ branches.join("  ") ++ "\n"' --color always | fzf --ansi | cut -f1)
-                    commandline -it "$name"
-                else
-                    commandline -it "# not in a jj directory"
-                end
-            end
-            bind \cj fzf_jj_edit
-        end
+        # if type -q fzf
+        #     function fzf_jj_edit
+        #         if test -d .jj
+        #             set -l name (jj log --no-graph -T 'change_id.shortest() ++ "\t" ++ description.first_line() ++ " "  ++ branches.join("  ") ++ "\n"' --color always | fzf --ansi | cut -f1)
+        #             commandline -it "$name"
+        #         else
+        #             commandline -it "# not in a jj directory"
+        #         end
+        #     end
+        #     bind \cj fzf_jj_edit
+        # end
     end
 end
