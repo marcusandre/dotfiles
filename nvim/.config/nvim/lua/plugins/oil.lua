514ad9deaf040fb271c79af2a9ca8b51dab01516
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-11-29 14:47:35 +0100

    nvim: update oil configuration

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,9 +5,10 @@ return {
     { "<leader>e", "<Cmd>Oil<CR>", desc = "Open file explorer" },
   },
   opts = {
+    columns = { "icon", "permissions", "size", "mtime" },
+    default_file_explorer = true,
     delete_to_trash = true,
-    view_options = {
-      show_hidden = true,
-    },
+    skip_confirm_for_simple_edits = true,
+    view_options = { show_hidden = true },
   },
 }
