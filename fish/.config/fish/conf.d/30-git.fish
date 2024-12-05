5d5b4cc91b8b1bc83874a74ba8ba71a139830dfe
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-12-05 17:12:34 +0100

    git: add gov abbreviation

--- fish/.config/fish/conf.d/30-git.fish
+++ fish/.config/fish/conf.d/30-git.fish
@@ -3,6 +3,7 @@ if status --is-interactive
         abbr -a gap "git add -p"
         abbr -a gd "git diff"
         abbr -a gl "git log --oneline --graph --decorate --branches"
+        abbr -a gov "git overwrite"
         abbr -a gp "git push"
         abbr -a gr "cd (git root)"
         abbr -a gs "git status --short"
