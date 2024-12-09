4b5251bee8ad7bbe4865c2b621887db824a87aa3
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-12-09 07:51:46 +0100

    fish: update EDITOR env

--- fish/.config/fish/conf.d/30-editor.fish
+++ fish/.config/fish/conf.d/30-editor.fish
@@ -1,9 +1,4 @@
-if type -q zed
-    abbr -a e zed --wait
-else if type -q nvim
+if type -q nvim
     set -gx EDITOR nvim
     abbr -a e nvim
-else if type -q hx
-    set -gx EDITOR hx
-    abbr -a e hx
 end
