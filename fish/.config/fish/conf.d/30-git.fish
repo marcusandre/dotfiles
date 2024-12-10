9342dbd827e4e00a2eb49a27774b355805b61728
Marcus André <102828+marcusandre@users.noreply.github.com>
2024-12-10 20:14:09 +0100

    fish: remove git-log helper

--- fish/.config/fish/conf.d/30-git.fish
+++ fish/.config/fish/conf.d/30-git.fish
@@ -2,7 +2,7 @@ if status --is-interactive
     if type -q git
         abbr -a gap "git add -p"
         abbr -a gd "git diff"
-        abbr -a gl "~/bin/git-log"
+        abbr -a gl "git log --oneline --graph --decorate --branches"
         abbr -a gov "git overwrite"
         abbr -a gp "git push"
         abbr -a gr "cd (git root)"
